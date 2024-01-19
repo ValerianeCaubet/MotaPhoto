4174fe0cf11f28f6ac590b162e7539bec9054ec8
Valeriane <valerianecaubet@gmail.com>
2024-01-19 06:04:05 +0100

    singlephoto non terminé

--- js/modale.js
+++ js/modale.js
@@ -1,21 +1,25 @@
-console.log(" modale.js est lancé ")
+console.log("modale.js est lancé");
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Sélectionnez les éléments nécessaires
-    var openModalButton = document.querySelector(".menu-item-27");
+    var openModalButton = document.querySelector(".menu-item-64");
     var modalOverlay = document.querySelector(".popup-overlay");
 
     // Ajoutez un gestionnaire d'événement pour le clic sur le bouton
-    openModalButton.addEventListener("click", function() {
+    openModalButton.addEventListener("click", function (event) {
+        // Empêcher le comportement par défaut (la redirection vers une autre page)
+        event.preventDefault();
+
         // Ajoutez la classe "open" à la modal overlay
         modalOverlay.classList.add("open");
     });
 
     // Ajoutez un gestionnaire d'événement pour le clic en dehors de la modal pour la fermer
-    modalOverlay.addEventListener("click", function(event) {
+    modalOverlay.addEventListener("click", function (event) {
         if (event.target === modalOverlay) {
             // Retirez la classe "open" pour masquer la modal
             modalOverlay.classList.remove("open");
         }
     });
 });
+
--- template-part/modale.php
+++ template-part/modale.php
@@ -3,7 +3,7 @@
 	<div class="popup-contact">
         <img src="<?php echo get_template_directory_uri() . '/assets/img/contact-header.png'; ?>" alt="contact">
 		<?php
-		echo do_shortcode('[contact-form-7 id="78078ec" title="Modale contact"]');
+		echo do_shortcode('[contact-form-7 id="b79bbfe" title="Modale Contact"]');
 		?>
 	</div>
 </div>

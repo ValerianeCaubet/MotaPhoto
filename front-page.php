729a7016ea89f3b4af48212a309149b7038f5206
Valeriane <valerianecaubet@gmail.com>
2024-01-31 03:08:28 +0100

    overlay ok

--- front-page.php
+++ front-page.php
@@ -1,3 +1,34 @@
 <?php get_header(); ?>
+<?php get_header(); ?>
 
+<!-- Hero -->
+<section class="hero">
+<?php
+    // arguments de la requête
+    $args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => 1, 
+        'orderby' => 'rand',
+    );
+
+    // création d' une nouvelle instance de WP_Query
+    $query = new WP_Query($args);
+
+    // boucle sur les résultats
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            ?>
+                <img src="<?php
+                    $photo = get_field('photo');
+                    echo $photo['url'];
+                    ?>" alt="photographie">
+            <?php
+        }
+    }
+    // réinitialisation de la requête
+    wp_reset_postdata();
+    ?>
+<h1>PHOTOGRAPHE EVENT</h1>
+</section>
 <?php get_footer(); ?>
